4e04e8f866837ac53cfb14161ea63bdf24dda5d0
Dzonatans Svilis-Sudints <Dzonatan2017@gmail.com>
2024-11-10 19:59:45 +0200

    test

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-<script>
-    AOS.init();
-</script>
+const answers = {
+    question1: 'CSS',
+    question2: 'JavaScript',
+    question3: 'SQL',
+    question4: 'PHP',
+    question5: 'JavaScript',
+    question6: 'HTML'
+};
+
+// Счётчик правильных ответов
+let correctAnswersCount = 0;
+const totalQuestions = 6;
+
+// Функция для проверки ответа
+function checkAnswer(questionId, selectedAnswer) {
+    const questionDiv = document.getElementById(questionId);
+    
+        if (selectedAnswer === answers[questionId]) {
+            questionDiv.classList.add('correct');
+            questionDiv.classList.remove('incorrect');
+            correctAnswersCount++;
+        } else {
+            questionDiv.classList.add('incorrect');
+            questionDiv.classList.remove('correct');
+            
+        }
+        
+        // Проверка, правильны ли все ответы
+        if (correctAnswersCount === totalQuestions) {
+            document.getElementById('result').textContent = 'Paldies par jūsu uzmanību!';
+        }
+    
+}
